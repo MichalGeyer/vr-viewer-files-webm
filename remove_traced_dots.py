267715804532fb2ba05598cae8b9863fcec41161
MichalGeyer <michal.geyer@gmail.com>
2025-01-13 12:49:46 +0100

    random order, subset of files

--- remove_traced_dots.py
+++ remove_traced_dots.py
@@ -1,22 +1,31 @@
 # iterate over folders in ./pairs-user-study-webm and remove traced dots from the folder names
 
 import os
-for folder in os.listdir('./pairs-user-study-webm'):
+root = './pairs-user-study-webm-ours-right/ours_to_the_right'
+for folder in os.listdir(root):
     if folder.endswith('.'):
         new_folder = folder[:-1]
-        os.rename('./pairs-user-study-webm/' + folder, './pairs-user-study-webm/' + new_folder)
-        print('Renamed ' + folder + ' to ' + new_folder)
-
-for folder in os.listdir('./pairs-user-study-images'):
-    if folder.endswith('.'):
-        new_folder = folder[:-1]
-        os.rename('./pairs-user-study-images/' + folder, './pairs-user-study-images/' + new_folder)
+        os.rename(f'{root}/' + folder, f'{root}/' + new_folder)
         print('Renamed ' + folder + ' to ' + new_folder)
 
 
-# itereate over pngs in './pairs-user-study-webm/images' and replace ..png with .png
-for file in os.listdir('./pairs-user-study-webm/images'):
-    if file.endswith('..png'):
-        new_file = file.replace('..png', '.png')
-        os.rename('./pairs-user-study-webm/images/' + file, './pairs-user-study-webm/images/' + new_file)
-        print('Renamed ' + file + ' to ' + new_file)
+
+# for folder in os.listdir('./pairs-user-study-webm'):
+#     if folder.endswith('.'):
+#         new_folder = folder[:-1]
+#         os.rename('./pairs-user-study-webm/' + folder, './pairs-user-study-webm/' + new_folder)
+#         print('Renamed ' + folder + ' to ' + new_folder)
+
+# for folder in os.listdir('./pairs-user-study-images'):
+#     if folder.endswith('.'):
+#         new_folder = folder[:-1]
+#         os.rename('./pairs-user-study-images/' + folder, './pairs-user-study-images/' + new_folder)
+#         print('Renamed ' + folder + ' to ' + new_folder)
+
+
+# # itereate over pngs in './pairs-user-study-webm/images' and replace ..png with .png
+# for file in os.listdir('./pairs-user-study-webm/images'):
+#     if file.endswith('..png'):
+#         new_file = file.replace('..png', '.png')
+#         os.rename('./pairs-user-study-webm/images/' + file, './pairs-user-study-webm/images/' + new_file)
+#         print('Renamed ' + file + ' to ' + new_file)
